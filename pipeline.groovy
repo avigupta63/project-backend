fa6a0deeffb37b8c4666a2e4d630f38e4089a866
avigupta63 <avi.gupta63@gmail.com>
2025-05-08 06:01:50 +0000

    changes in script

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,24 +1,28 @@
 pipeline {
     agent any
     stages {
-        stage('code-pull'){
+        stage('code-pull') {
             steps {
                 git branch: 'dev', url: 'https://github.com/avigupta63/project-backend.git'
             }
         }
-        stage('code-build'){
+        stage('code-build') {
             steps {
                 sh 'mvn clean package'
             }
         }
-        stage('deploy-k8s'){
+        stage('docker-build-push') {
             steps {
                 sh '''
                     docker build . -t avigupta63/project-backend-img:latest
                     docker push avigupta63/project-backend-img:latest
                     docker rmi avigupta63/project-backend-img:latest
-                    kubectl apply -f ./deploy/
                 '''
+            }
+        }
+        stage('deploy-k8s') {
+            steps {
+                sh 'kubectl apply -f ./deploy/'
             }
         }
     }
